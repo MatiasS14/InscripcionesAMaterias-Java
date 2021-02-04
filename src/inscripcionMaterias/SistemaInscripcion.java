575e026d09ed16c36fa346584b7b74e50b15758e
Matias Solanes <matias.solanes14@gmail.com>
2021-02-03 23:40:40 -0300

    SistemaInscripcion agregado

--- src/inscripcionMaterias/SistemaInscripcion.java
+++ src/inscripcionMaterias/SistemaInscripcion.java
@@ -1,5 +1,49 @@
 package inscripcionMaterias;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Queue;
 
 public class SistemaInscripcion {
+	private Set<Carrera> carreras;
+	
+	public SistemaInscripcion() {
+		this.carreras = new HashSet<Carrera>();
+	}
+	
+	public void agregarCarrera(Carrera carrera) {
+		this.carreras.add(carrera);
+	}
+	
+	public Set<Estudiante> alumnosInscriptos(Materia materia){
+		return materia.alumnosInscriptos();
+	}
+	public Queue<Estudiante> alumnosEnEspera(Materia materia){
+	   	return materia.listaDeEspera();
+	}
+	
+	public Set<Materia> materiasParaCursar(Estudiante est, Carrera carrera){
+	  return carrera.materiasParaCursas(est);
+	}
+	
+	public Set<Carrera> carreras(){
+		return this.carreras;
+	}
+	
+	public Set<Materia> materiasAlumnoInscripto(Estudiante alu){
+		return alu.materiasInscripto();
+	}
+	
+	public Set<Materia> materiasAlumnoEnEspera(Estudiante alu){
+		return materiasEnEspera(alu, alu.materiasInscripto());
+	}
 
+	private Set<Materia> materiasEnEspera(Estudiante alu, Set<Materia> materias) {
+		Set<Materia> ret = new HashSet<Materia>();
+		for(Materia mat : materias) {
+			if(mat.listaDeEspera().contains(alu)) {
+				ret.add(mat);
+			}
+		}
+	 return ret;
+	}
 }
