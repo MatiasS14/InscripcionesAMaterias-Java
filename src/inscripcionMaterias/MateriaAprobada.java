15f82f55610d222e4644edb18e49e9ccb8d1116c
Matias Solanes <matias.solanes14@gmail.com>
2021-02-03 23:48:53 -0300

    Materia aprobada agregada

--- src/inscripcionMaterias/MateriaAprobada.java
+++ src/inscripcionMaterias/MateriaAprobada.java
@@ -1,5 +1,31 @@
 package inscripcionMaterias;
 
 public class MateriaAprobada {
-
+	private String materia;
+	private Integer nota;
+	private String alumno;
+	private Integer añoMateria;
+	
+	public MateriaAprobada(String nombreMateria, Integer nota, String nombreAlumno, Integer año) {
+		this.materia = nombreMateria;
+		this.nota = nota;
+		this.alumno = alumno;
+		this.añoMateria = año;
+	}
+	
+	public String materia() {
+		return this.materia;
+	}
+	
+	public Integer nota() {
+		return this.nota;
+	}
+	
+	public String alumno() {
+		return this.alumno;
+	}
+	
+	public Integer año() {
+		return this.añoMateria;
+	}
 }
